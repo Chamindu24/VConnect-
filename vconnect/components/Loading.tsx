7ff82e1c42c5c066adcda0e4ad5b9d49dda87d71
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-03-19 23:40:20 +0530

    refactor Loading component layout; update Next.js config for remote image patterns

--- vconnect/components/Loading.tsx
+++ vconnect/components/Loading.tsx
@@ -1,11 +1,9 @@
-import {Mosaic} from "react-loading-indicators"
+import { Mosaic } from "react-loading-indicators";
 
 const Loading = () => {
   return (
-    <div className="flex flex-col items-center animate-fade-in pt-16">
-      
-      <Mosaic color={["#33CCCC", "#33CC36", "#B8CC33", "#FCCA00"]} />    
-
+    <div className="flex items-center justify-center min-h-screen animate-fade-in">
+      <Mosaic color={["#33CCCC", "#33CC36", "#B8CC33", "#FCCA00"]} />
     </div>
   );
 };
