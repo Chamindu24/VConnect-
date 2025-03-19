79781d45f6bf5af11b1ec26cb1cf8bce31a60acf
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-03-19 20:58:20 +0530

    add loading component, date and time display, and menu item card; update styles and layout

--- vconnect/app/(auth)/(main)/(home)/page.tsx
+++ vconnect/app/(auth)/(main)/(home)/page.tsx
@@ -1,11 +1,11 @@
-//import MainMenu from "@/components/MainMenu"
-//import StatusBar from "@/components/StatusBar"
+import MainMenu from "@/components/MainMenu"
+import StatusBar from "@/components/StatusBar"
 
 const HomePage = () => {
     return (
         <div className="flex flex-col gap-32 pt-20 pl-10 items-center max-md:gap-10 md:flex-row animate-fade-in">
-            {/*<StatusBar/>
-            <MainMenu/>*/}
+           {/*<StatusBar/>*/ } 
+           <MainMenu/>
             <p>lets talk</p>
         </div>
     )
